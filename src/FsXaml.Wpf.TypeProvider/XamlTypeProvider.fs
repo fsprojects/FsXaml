21dde94cb61b7e6bd48ebfc81e1762b6a45f28a9
ReedCopsey <reed@reedcopsey.com>
2014-03-14 12:46:37 -0700

    Updated version to 0.9.1
    Updated version for new NuGet package

--- src/FsXaml.Wpf.TypeProvider/XamlTypeProvider.fs
+++ src/FsXaml.Wpf.TypeProvider/XamlTypeProvider.fs
@@ -21,8 +21,8 @@ open FsXaml.TypeProviders.Helper
 
 open System.Reflection
 
-[<assembly:AssemblyVersion("0.9.0.0")>]
-[<assembly:AssemblyFileVersion("0.9.0.0")>]
+[<assembly:AssemblyVersion("0.9.1.0")>]
+[<assembly:AssemblyFileVersion("0.9.1.0")>]
 do()
 
 
