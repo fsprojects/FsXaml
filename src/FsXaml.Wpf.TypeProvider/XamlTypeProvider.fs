40aa2394a2c3b91df2865686a1a8580c54f54b71
ReedCopsey <reed@reedcopsey.com>
2014-03-18 12:23:11 -0700

    Updated version to 0.9.3

--- src/FsXaml.Wpf.TypeProvider/XamlTypeProvider.fs
+++ src/FsXaml.Wpf.TypeProvider/XamlTypeProvider.fs
@@ -21,8 +21,8 @@ open FsXaml.TypeProviders.Helper
 
 open System.Reflection
 
-[<assembly:AssemblyVersion("0.9.2.0")>]
-[<assembly:AssemblyFileVersion("0.9.2.0")>]
+[<assembly:AssemblyVersion("0.9.3.0")>]
+[<assembly:AssemblyFileVersion("0.9.3.0")>]
 do()
 
 
--- src/FsXaml.Wpf/Contracts.fs
+++ src/FsXaml.Wpf/Contracts.fs
@@ -8,8 +8,8 @@ open Microsoft.FSharp.Quotations.Patterns
 
 open System.Reflection
 
-[<assembly:AssemblyVersion("0.9.2.0")>]
-[<assembly:AssemblyFileVersion("0.9.2.0")>]
+[<assembly:AssemblyVersion("0.9.3.0")>]
+[<assembly:AssemblyFileVersion("0.9.3.0")>]
 do()
 
 /// Used for one-way conversion from EventArgs -> other types when used with EventToCommand 
