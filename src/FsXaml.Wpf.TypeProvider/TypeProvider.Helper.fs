cff5b83601f9e290104d75bd97e082045ecff769
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2014-06-11 15:40:09 +0400

    dispose FileSystemWatcher in TP.Dispose() method

--- src/FsXaml.Wpf.TypeProvider/TypeProvider.Helper.fs
+++ src/FsXaml.Wpf.TypeProvider/TypeProvider.Helper.fs
@@ -21,13 +21,14 @@ let optionType ty = typedefof<option<_>>.MakeGenericType[| ty |]
 
 /// Implements invalidation of schema when the file changes
 let watchForChanges (ownerType:TypeProviderForNamespaces) (fileName:string) = 
-    if fileName.StartsWith("http", System.StringComparison.InvariantCultureIgnoreCase) then () else
+    if fileName.StartsWith("http", System.StringComparison.InvariantCultureIgnoreCase) then None else
 
     let path = Path.GetDirectoryName(fileName)
     let name = Path.GetFileName(fileName)
     let watcher = new FileSystemWatcher(Filter = name, Path = path)
     watcher.Changed.Add(fun _ -> ownerType.Invalidate())
     watcher.EnableRaisingEvents <- true
+    Some (watcher :> IDisposable) 
 
 // Get the assembly and namespace used to house the provided types
 let thisAssembly = System.Reflection.Assembly.GetExecutingAssembly()
