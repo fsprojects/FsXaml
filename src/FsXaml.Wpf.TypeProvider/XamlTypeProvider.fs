2b7608402268d9366a73fcfef85aa8c7dbad1eb2
ReedCopsey <reed@reedcopsey.com>
2014-06-12 09:44:17 -0700

    Updated version to 0.9.4

--- src/FsXaml.Wpf.TypeProvider/XamlTypeProvider.fs
+++ src/FsXaml.Wpf.TypeProvider/XamlTypeProvider.fs
@@ -22,8 +22,8 @@ open FsXaml.TypeProviders.Helper
 
 open System.Reflection
 
-[<assembly:AssemblyVersion("0.9.3.0")>]
-[<assembly:AssemblyFileVersion("0.9.3.0")>]
+[<assembly:AssemblyVersion("0.9.4.0")>]
+[<assembly:AssemblyFileVersion("0.9.4.0")>]
 do()
 
 
