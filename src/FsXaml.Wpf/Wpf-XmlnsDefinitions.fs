e80368c46237a1d1efc0d077ae4c15dd9c18f7f3
ReedCopsey <reed@reedcopsey.com>
2014-06-16 17:25:45 -0700

    Updated to latest FSharp.ViewModule
    Added validation to SimpleMvvm application

---
+++ src/FsXaml.Wpf/Wpf-XmlnsDefinitions.fs
@@ -0,0 +1,8 @@
+﻿namespace FsXaml
+
+open System.Windows.Markup
+
+
+[<assembly: XmlnsPrefix("http://github.com/fsprojects/FsXaml", "fsxaml")>]
+[<assembly: XmlnsDefinition("http://github.com/fsprojects/FsXaml", "FsXaml")>]
+do()
