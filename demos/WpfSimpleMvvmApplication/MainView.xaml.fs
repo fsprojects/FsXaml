d8e0e71627696888c76d484c33746b0096326dde
ReedCopsey <reed@reedcopsey.com>
2014-10-09 13:08:08 -0700

    Added ViewController on UserControl for demo
    SimpleMvvmApplication UserControl now has a ViewController sample

--- demos/WpfSimpleMvvmApplication/MainView.xaml.fs
+++ demos/WpfSimpleMvvmApplication/MainView.xaml.fs
@@ -4,3 +4,19 @@ open FsXaml
 
 type MainView = XAML<"MainView.xaml">
 
+// This is to demonstrate being able to add in "code behind"
+// Note, in this case, this only displays a message when double clicking on 
+// the full name text box
+type MainViewController() =
+
+    let showMessage _ =
+        System.Windows.MessageBox.Show "You double clicked on Full Name!"
+        |> ignore
+
+    interface IViewController with
+        member this.Attach fe =
+            // Use the TypeProvider's Accessor sub-type to gain access to named members
+            let view = MainView.Accessor fe
+            // Subscribe to the double click event
+            view.tbFullName.MouseDoubleClick.Add showMessage
+
