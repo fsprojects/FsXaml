aad5eb15ee2892831b3c5da9bdf2b710b8d4b388
ReedCopsey <reed@reedcopsey.com>
2016-05-18 12:42:49 -0700

    minor work on docs

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -2,7 +2,8 @@
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
 #I "../../bin"
-
+#r "FsXaml.Wpf.TypeProvider\FsXaml.Wpf.dll"
+#r "FsXaml.Wpf.TypeProvider\FsXaml.Wpf.TypeProvider.dll"
 (**
 FsXaml
 ======================
@@ -13,8 +14,8 @@ Documentation
   <div class="span1"></div>
   <div class="span6">
     <div class="well well-small" id="nuget">
-      The FsXaml library can be <a href="https://nuget.org/packages/FsXaml">installed from NuGet</a>:
-      <pre>PM> Install-Package FsXaml</pre>
+      The FsXaml library can be <a href="https://nuget.org/packages/FsXaml.Wpf">installed from NuGet</a>:
+      <pre>PM> Install-Package FsXaml.Wpf</pre>
     </div>
   </div>
   <div class="span1"></div>
@@ -23,29 +24,22 @@ Documentation
 Example
 -------
 
-This example demonstrates using a function defined in this sample library.
+This example demonstrates using the XAML type provider to create a UI.
 
 *)
-#r "FsXaml.dll"
 open FsXaml
 
-printfn "hello = %i" <| Library.hello 0
+type MainWindow = XAML<"MainWindow.xaml">
 
 (**
-Some more info
 
 Samples & documentation
 -----------------------
 
-The library comes with comprehensible documentation. 
-It can include tutorials automatically generated from `*.fsx` files in [the content folder][content]. 
-The API reference is automatically generated from Markdown comments in the library implementation.
-
  * [Tutorial](tutorial.html) contains a further explanation of this sample library.
 
  * [API Reference](reference/index.html) contains automatically generated documentation for all types, modules
-   and functions in the library. This includes additional brief samples on using most of the
-   functions.
+   and functions in the library. 
  
 Contributing and copyright
 --------------------------
@@ -55,7 +49,7 @@ the project and submit pull requests. If you're adding a new public API, please 
 consider adding [samples][content] that can be turned into a documentation. You might
 also want to read the [library design notes][readme] to understand how it works.
 
-The library is available under Public Domain license, which allows modification and 
+The library is available under the Apache 2.0 license, which allows modification and 
 redistribution for both commercial and non-commercial purposes. For more information see the 
 [License file][license] in the GitHub repository. 
 
--- docs/content/tutorial.fsx
+++ docs/content/tutorial.fsx
@@ -2,18 +2,26 @@
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
 #I "../../bin"
+#r "FsXaml.Wpf.TypeProvider\FsXaml.Wpf.dll"
+#r "FsXaml.Wpf.TypeProvider\FsXaml.Wpf.TypeProvider.dll"
 
 (**
-Introducing your project
+FsXaml
 ========================
 
-Say more
+The FsXaml for WPF library and type provider allows usage of XAML for WPF projects directly from F#. It eliminates the need
+to create C# projects when writing WPF Applications or libraries.
+
+This is done via a XAML type provider:
 
 *)
-#r "FsXaml.dll"
+
 open FsXaml
 
-Library.hello 0
+type MainWindow = XAML<"MainWindow.xaml">
+
 (**
-Some more info
+
+Usage and instructions to come shortly.
+
 *)
