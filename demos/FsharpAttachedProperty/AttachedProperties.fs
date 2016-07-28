f88d4c2504fb5f411eeecb2d66d33e037e769a45
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-28 20:16:31 +0200

    Add sample with attached property in module.

---
+++ demos/FsharpAttachedProperty/AttachedProperties.fs
@@ -0,0 +1,17 @@
+﻿namespace FsharpAttachedProperty
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Media
+
+module Icon = 
+    // http://stackoverflow.com/a/14706890/1069200
+    type internal Marker = interface end
+
+    let GeometryProperty = DependencyProperty.RegisterAttached("Geometry", typeof<Geometry>, typeof<Marker>.DeclaringType, PropertyMetadata(null :> Geometry))
+    
+    let SetGeometry (element: FrameworkElement, value : Geometry) = 
+        element.SetValue(GeometryProperty, value)
+    
+    let GetGeometry (element: FrameworkElement) : Geometry = 
+        element.GetValue(GeometryProperty) :?> _
+
