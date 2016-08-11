5e2116626b8f1bf4524dafb9e623e8a0bdee1f4e
JohanLarsson <johan_gavle1@hotmail.com>
2016-08-11 22:08:01 +0200

    UI-test WpfMvvmAgent
    - Just asserting that it builds & runs.

---
+++ FsXaml.UiTests/WpfMvvmAgentTests.fs
@@ -0,0 +1,18 @@
+﻿module FsXaml.UiTests.WpfMvvmAgentTests
+
+open System
+open System.Diagnostics
+
+open NUnit.Framework
+open TestStack.White
+open TestStack.White.UIItems
+
+let startInfo = StartInfo.create @"..\..\..\demos\WpfMvvmAgent\bin\Release\WpfMvvmAgent.exe"
+
+[<Test>]
+let ``loads``() = 
+    Assert.True(System.IO.File.Exists(startInfo.FileName))
+    // not a very useful test, just tests that nothing is horribly wrong
+    use application = Application.AttachOrLaunch(startInfo)
+    let window = application.GetWindow("MVVM With Agents")
+    Assert.NotNull(window.Get<Button>("ExitButton"))
