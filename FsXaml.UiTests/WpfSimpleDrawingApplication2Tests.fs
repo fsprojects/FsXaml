22d70fddabb3519ee458a6b15c9cecdca9c62dcd
JohanLarsson <johan_gavle1@hotmail.com>
2016-08-11 22:20:44 +0200

    UI-test WpfSimpleDrawingApplication2
    - Just testing that it loads and does not explode if we click.

---
+++ FsXaml.UiTests/WpfSimpleDrawingApplication2Tests.fs
@@ -0,0 +1,27 @@
+﻿module FsXaml.UiTests.WpfSimpleDrawingApplication2Tests
+
+open System
+open System.Diagnostics
+
+open NUnit.Framework
+open TestStack.White
+open TestStack.White.UIItems
+open TestStack.White.UIItems.Finders
+
+let startInfo = StartInfo.create @"..\..\..\demos\WpfSimpleDrawingApplication2\bin\Release\WpfSimpleDrawingApplication2.exe"
+
+[<Test>]
+let ``load and click charts``() = 
+    Assert.True(System.IO.File.Exists(startInfo.FileName))
+    // not a very useful test, just tests that nothing is horribly wrong
+    use application = Application.AttachOrLaunch(startInfo)
+    let window = application.GetWindow("Drawing using Handler")
+
+    let functionChart = window.Get<GroupBox>(SearchCriteria.ByText("fsxaml:Function map"))
+    functionChart.Click()
+
+    let propertyChart = window.Get<GroupBox>(SearchCriteria.ByText("x:Static map"))
+    propertyChart.Click()
+
+    // checking that we did not crash.
+    Assert.NotNull(window)
