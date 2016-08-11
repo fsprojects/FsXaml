e2e9d79c91c3b455a204f5ad42d44dfda7398011
JohanLarsson <johan_gavle1@hotmail.com>
2016-08-11 21:39:07 +0200

    Change title FsharpAttachedProperty, hide the copy-paste.

--- demos/FsharpAttachedProperty/AssemblyInfo.fs
+++ demos/FsharpAttachedProperty/AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 (* General Information about an assembly is controlled through the following 
 set of attributes. Change these attribute values to modify the information
 associated with an assembly. *)
-[<assembly: AssemblyTitle("FSharpDesktopUINumericUpDown")>]
+[<assembly: AssemblyTitle("FsharpAttachedProperty")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("FSharpDesktopUINumericUpDown")>]
+[<assembly: AssemblyProduct("FsharpAttachedProperty")>]
 [<assembly: AssemblyCopyright("Copyright ©  2014")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
