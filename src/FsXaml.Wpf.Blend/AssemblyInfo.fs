c7a20381acb852f6337d8efa190fb16df057fd79
ReedCopsey <reed@reedcopsey.com>
2017-02-02 17:23:50 -0800

    Bumped version

--- src/FsXaml.Wpf.Blend/AssemblyInfo.fs
+++ src/FsXaml.Wpf.Blend/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FsXaml.Wpf.Blend")>]
 [<assembly: AssemblyProductAttribute("FsXaml")>]
 [<assembly: AssemblyDescriptionAttribute("F# Tools for working with XAML Projects")>]
-[<assembly: AssemblyVersionAttribute("3.1.1")>]
-[<assembly: AssemblyFileVersionAttribute("3.1.1")>]
+[<assembly: AssemblyVersionAttribute("3.1.2")>]
+[<assembly: AssemblyFileVersionAttribute("3.1.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FsXaml.Wpf.Blend"
     let [<Literal>] AssemblyProduct = "FsXaml"
     let [<Literal>] AssemblyDescription = "F# Tools for working with XAML Projects"
-    let [<Literal>] AssemblyVersion = "3.1.1"
-    let [<Literal>] AssemblyFileVersion = "3.1.1"
+    let [<Literal>] AssemblyVersion = "3.1.2"
+    let [<Literal>] AssemblyFileVersion = "3.1.2"
